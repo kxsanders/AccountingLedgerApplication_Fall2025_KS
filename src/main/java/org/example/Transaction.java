6dc6e0cfbf5b54950eb5c7f36a8c8634b75dc2b4
kxsanders <kxsanders98@gmail.com>
2025-10-10 10:36:52 -0500

    Added all classes I believe I will need. I finished the Transaction class, which is a POJO.

---
+++ src/main/java/org/example/Transaction.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    //this is a POJO class
+    //Time is not a string, Java has a built-in for dates and times
+    private LocalDate date;
+    private LocalTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    //Constructor
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    //Getters and Setters
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
+
+//may need to make own toString override, will see once I have more data
+
